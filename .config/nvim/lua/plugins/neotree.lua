b9af2d0a5657065ac57326ca297dbf812a3446e1
Yevhen Shulha <jackshel45@gmail.com>
2024-02-23 13:39:08 +0200

    fix: neotree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -3,11 +3,34 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",   -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
     vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>')
-  end
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_by_name = {
+            ".git",
+            ".DS_Store"
+          }
+        }
+      },
+      window = {
+        mappings = {
+          ['Y'] = function(state)
+            -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+            -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            vim.fn.setreg('*', filepath)
+            vim.notify("Copied: " .. filepath)
+          end
+        }
+      },
+    })
+  end,
 }
